d3cdea3675a4b53e0256f691325996f43046c07a
Lee Broadhurst <lee.broadhurst@madetech.com>
2022-02-14 17:13:58 +0000

    wrote first three tests and methods

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,1 +1,51 @@
 require_relative '../tic_tac_toe.rb'
+require 'stringio'
+
+describe TicTacToe do
+    describe 'score_board' do 
+        it 'Can return an array with 9 empty strings' do
+            tic_tac_toe = described_class.new
+        
+            expect(tic_tac_toe.score_board).to eq (['', '', '',
+        '', '', '',
+        '', '', ''])
+        end
+    end
+    describe 'display_board' do
+        it 'Can display a 3 x 3 grid in the terminal' do
+            tic_tac_toe = described_class.new
+            row = "   " "|" "   " "|" "   \n"
+            separator = "----------- \n"
+            expect{tic_tac_toe.display_board}.to output("   " "|" "   " "|" "   \n" "-----------\n" "   " "|" "   " "|" "   \n" "-----------\n" "   " "|" "   " "|" "   \n").to_stdout
+        end
+    
+    end
+    
+
+    describe 'Update user' do
+        it 'Can receive user input and change user name' do
+            $stdin = StringIO.new('Lee')
+            user_name = UserName.new('Not defined')
+            tic_tac_toe = described_class.new
+            
+            expect{tic_tac_toe.change_user_name(user_name)}. to output("Name: \n").to_stdout.and change {user_name.name}.to('Lee')
+        end
+    end
+    
+    
+
+
+    # it '' do
+    #     tic_tac_toe = described_class.new
+        
+    #     expect()
+    # end
+
+ 
+end
+
+# Can return an array of 9 empty strings
+# Can display empty grid to the terminal
+
+# Can take user input an populate one string in the array
+# Display updated grid with player 1's input
---
+++ tic_tac_toe.rb
@@ -0,0 +1,45 @@
+class UserName
+    attr_accessor :name
+    def initialize (name = 'Not defined')
+        @name = name
+    end
+end
+
+
+
+
+class TicTacToe 
+    def score_board
+    score_array = ['', '', '',
+    '', '', '',
+    '', '', '']
+    end
+
+    def display_board
+        row =  ["   " "|" "   " "|" "   "]
+        separator = "-----------"
+
+        puts row
+        puts separator
+        puts row
+        puts separator
+        puts row        
+    end
+
+    def change_user_name(username_instance)
+        puts username_instance.name
+        puts "Name: "
+        username_instance.name = gets.chomp
+        puts "You are playing as #{username_instance.name }"
+    end
+
+  end
+
+
+
+example =  TicTacToe.new
+puts example.change_user_name(UserName.new())
+
+
+
+
